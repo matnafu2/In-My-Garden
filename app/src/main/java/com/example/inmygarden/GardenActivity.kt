85188c086d9a4f4456b3a5006ae871ab69bffce9
Sai Gopinath <66979842+saigopinath05@users.noreply.github.com>
2022-12-03 15:26:31 -0500

    implemented sharedPreferences, added functionality to clear garden

--- app/src/main/java/com/example/inmygarden/GardenActivity.kt
+++ app/src/main/java/com/example/inmygarden/GardenActivity.kt
@@ -1,6 +1,8 @@
 package com.example.inmygarden
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -12,8 +14,12 @@ import com.example.inmygarden.databinding.ActivityGardenBinding
 class GardenActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGardenBinding
     private lateinit var flowerArray: Array<ImageView>
+    private lateinit var visibilityArray: Array<Boolean>
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPreferences = this.getSharedPreferences("application", Context.MODE_PRIVATE)
         binding = ActivityGardenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -21,27 +27,75 @@ class GardenActivity : AppCompatActivity() {
             val returnToMainActivity = Intent(this, MainActivity::class.java)
             startActivity(returnToMainActivity)
         }
+        binding.clearGarden.setOnClickListener{
+            clearGarden()
+        }
         flowerArray = arrayOf(binding.flower1, binding.flower2, binding.flower3, binding.flower4,
             binding.flower5, binding.flower6, binding.flower7, binding.flower8, binding.flower9)
+        pullFromSharedPreferences()
         observePlant()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        pullFromSharedPreferences()
     }
     private fun observePlant() {
         gardenViewModel.plantFinished.observe(this) {
             if (it) {
+                var check = false
                 for (flower in flowerArray) {
                     if (flower.visibility == View.INVISIBLE) {
                         flower.visibility = View.VISIBLE
                         makeToast()
-                        Log.i("IMG", "flower set to visible")
+                        updateSharedPreferences(flower.id.toString(), true)
+                        //Log.i("IMG", "flower set to visible")
+                        check = true
                         break
                     }
                 }
+                if (check == false) makeErrorToast()
             }
         }
     }
     private fun makeToast() {
         Toast.makeText(applicationContext, "Your plant has been added to the garden!",
             Toast.LENGTH_SHORT).show()
+    }
+    private fun makeErrorToast() {
+        Toast.makeText(applicationContext, "Your garden is full! Clear garden to make space",
+            Toast.LENGTH_SHORT).show()
+    }
+    private fun makeClearToast() {
+        Toast.makeText(applicationContext, "Cleared Garden",
+            Toast.LENGTH_SHORT).show()
+    }
+    private fun updateSharedPreferences(id: String, value: Boolean) {
+        val editor = sharedPreferences.edit()
+        editor.putBoolean(id, value)
+        editor.apply()
+        Log.i("IMG", "$id is stored as visible")
+    }
+    private fun pullFromSharedPreferences() {
+        for (flower in flowerArray) {
+            if (sharedPreferences.getBoolean(flower.id.toString(), false)) {
+                Log.i("IMG", "${(flower.id.toString())} is visible ")
+                flower.visibility = View.VISIBLE
+            }
+            else {
+                Log.i("IMG", "${(flower.id.toString())} is invisible ")
+            }
+        }
+    }
+
+    private fun clearGarden() {
+        val editor = sharedPreferences.edit()
+        for (flower in flowerArray) {
+            flower.visibility = View.INVISIBLE
+            editor.putBoolean(flower.id.toString(), false)
+        }
+        makeClearToast()
+        editor.apply()
     }
     companion object {
         val gardenViewModel = GardenViewModel()
