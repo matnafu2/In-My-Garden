67cb8cd822e8709c2642b93199f1268108d84dc9
Sai Gopinath <66979842+saigopinath05@users.noreply.github.com>
2022-12-04 16:53:41 -0500

    added functionality to change pot colors

--- app/src/main/java/com/example/inmygarden/GardenActivity.kt
+++ app/src/main/java/com/example/inmygarden/GardenActivity.kt
@@ -3,12 +3,16 @@ package com.example.inmygarden
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
+import android.graphics.ColorFilter
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.inmygarden.databinding.ActivityGardenBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -21,15 +25,14 @@ import com.google.firebase.ktx.Firebase
 class GardenActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGardenBinding
     private lateinit var flowerArray: Array<ImageView>
-    private lateinit var database: DatabaseReference
+    private lateinit var potArray: Array<ImageView>
+    //private lateinit var database: DatabaseReference
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var userId: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        database = Firebase.database.reference
-        userId = FirebaseAuth.getInstance().uid.toString()
-        Log.i("IMG", userId)
-        fetchData()
+        //database = Firebase.database.reference
+        //userId = FirebaseAuth.getInstance().uid.toString()
         sharedPreferences = this.getSharedPreferences("application", Context.MODE_PRIVATE)
         binding = ActivityGardenBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -38,26 +41,63 @@ class GardenActivity : AppCompatActivity() {
             val returnToMainActivity = Intent(this, MainActivity::class.java)
             startActivity(returnToMainActivity)
         }
+        binding.changeColor.setOnClickListener {
+            changePotColors()
+        }
         binding.clearGarden.setOnClickListener{
             clearGarden()
         }
         flowerArray = arrayOf(binding.flower1, binding.flower2, binding.flower3, binding.flower4,
             binding.flower5, binding.flower6, binding.flower7, binding.flower8, binding.flower9)
+
+        potArray = arrayOf(binding.pot1, binding.pot2, binding.pot3, binding.pot4,
+            binding.pot5, binding.pot6, binding.pot7, binding.pot8, binding.pot9)
+
         pullFromSharedPreferences()
         observePlant()
     }
-    private fun fetchData() {
-        database.child("users")
-        database.child("users").child(userId).get().addOnSuccessListener {
-            Log.i("firebase", "Got value ${it.value}")
-        }.addOnFailureListener{
-            Log.e("firebase", "Error getting data", it)
-        }
-    }
-
     override fun onResume() {
         super.onResume()
         pullFromSharedPreferences()
+    }
+    // when the "change pot colors" is clicked, user then has to click on the pot
+    // they want to change, then an overlay view is made visible in which they
+    // can select the color they want
+    private fun changePotColors() {
+        Toast.makeText(applicationContext, "Click on a pot to change its color!",
+            Toast.LENGTH_SHORT).show()
+        for (pot in potArray) {
+            pot.setOnClickListener {
+                binding.overlay.visibility = View.VISIBLE
+                binding.overlay.bringToFront()
+                binding.clearGarden.visibility = View.INVISIBLE
+                binding.changeColor.visibility = View.INVISIBLE
+                binding.blue.setOnClickListener {
+                    binding.potSample.setColorFilter(Color.CYAN, android.graphics.PorterDuff.Mode.MULTIPLY)
+                    pot.setColorFilter(Color.CYAN, android.graphics.PorterDuff.Mode.MULTIPLY)
+                }
+                binding.red.setOnClickListener {
+                    binding.potSample.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.MULTIPLY)
+                    pot.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.MULTIPLY)
+                }
+                binding.yellow.setOnClickListener {
+                    binding.potSample.setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.MULTIPLY)
+                    pot.setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.MULTIPLY)
+                }
+                binding.brown.setOnClickListener {
+                    pot.clearColorFilter()
+                    binding.potSample.clearColorFilter()
+                }
+            }
+        }
+        binding.applyChanges.setOnClickListener {
+            binding.potSample.clearColorFilter()
+            binding.overlay.visibility = View.INVISIBLE
+            binding.clearGarden.visibility = View.VISIBLE
+            binding.changeColor.visibility = View.VISIBLE
+
+        }
+
     }
     private fun observePlant() {
         gardenViewModel.plantFinished.observe(this) {
